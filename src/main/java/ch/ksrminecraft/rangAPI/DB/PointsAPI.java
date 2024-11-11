74a43a5b487c90ee67e6d829d389af8e8d60ebb5
Samuel Huwiler <Samuel_huwiler@sluz.ch>
2024-11-11 16:58:23 +0100

    added Update and Get wrappers for SQL query (internal methods for Select and Update Query types

--- src/main/java/ch/ksrminecraft/rangAPI/DB/PointsAPI.java
+++ src/main/java/ch/ksrminecraft/rangAPI/DB/PointsAPI.java
@@ -1,9 +1,30 @@
 package ch.ksrminecraft.rangAPI.DB;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class PointsAPI {
     Connection connection;
     public PointsAPI(Connection conn) {
         connection = conn;
+    }
+    private String getSQL(String command) {
+        try (PreparedStatement ps = connection.prepareStatement(command)) {
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return rs.getString("name");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    private void SQLUpdate(String command){
+        try (PreparedStatement ps = connection.prepareStatement(command)) {
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/ch/ksrminecraft/rangAPI/RangAPI.java
+++ src/main/java/ch/ksrminecraft/rangAPI/RangAPI.java
@@ -1,18 +1,28 @@
 package ch.ksrminecraft.rangAPI;
 
 import ch.ksrminecraft.rangAPI.DB.Database;
+import ch.ksrminecraft.rangAPI.DB.PointsAPI;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class RangAPI extends JavaPlugin {
+import java.awt.*;
+import java.sql.Connection;
 
+public final class RangAPI extends JavaPlugin {
+    Database dbConnector = new Database();
+    Connection connection;
     @Override
     public void onEnable() {
-        Database dbConnector = new Database();
         dbConnector.connect("jdbc:mysql://HOST/DATABASE_NAME", "USERNAME", "PASSWORD");
+        connection = dbConnector.getConnection();
+
     }
 
     @Override
     public void onDisable() {
         //TODO DBSession Teardown
     }
+
+    //public void GetPoints();
+    //public void SetPoints();
+    //public void AddPoints();
 }
